205c16dfae9859d8b946f4913143afded5b05241
Sherif Nafee <shico.m.nafee@gmail.com>
2022-02-21 18:05:11 +0300

    Add Finished detection

---
+++ Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FinishTrigger : MonoBehaviour
+    {
+        [SerializeField] private string _playerTag = "Player";
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(_playerTag)) return;
+            
+            Debug.Log("Finish");
+            other.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+}
