7f3aaa338894c3d969b24c71d68716a5cacaf686
Sherif Nafee <shico.m.nafee@gmail.com>
2022-02-21 17:28:08 +0300

    Add simple coin destruction script

---
+++ Assets/Scripts/SimpleCoin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SimpleCoin : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
